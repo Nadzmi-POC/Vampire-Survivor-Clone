a7be61d9fe6130a86d1e947c07c17574060797b8
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2022-03-13 23:19:55 +0800

    rollback changes for enemy damage controller

--- Assets/script/enemy/EnemyDamageController.cs
+++ Assets/script/enemy/EnemyDamageController.cs
@@ -15,13 +15,7 @@ public class EnemyDamageController : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Player player = collision.GetComponent<Player>();
-
-        if (player != null)
-        {
-            Debug.Log(JsonUtility.ToJson(player, true));
-            HPController hpController = collision.GetComponent<HPController>();
-            hpController?.Damage(stat.attack);
-        }
+        HPController hpController = collision.GetComponent<HPController>();
+        hpController?.Damage(stat.attack);
     }
 }
